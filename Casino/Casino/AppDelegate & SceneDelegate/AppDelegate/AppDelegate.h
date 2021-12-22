3885a444393499bd35a3e6b120de59263b5e3a12
Jobs <lg295060456@vip.qq.com>
2021-12-22 21:29:27 +0700

    first commit

---
+++ Casino/Casino/AppDelegate & SceneDelegate/AppDelegate/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Casino
+//
+//  Created by Jobs on 2021/12/14.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong) NSPersistentCloudKitContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Casino/Casino/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Casino
+//
+//  Created by Jobs on 2021/12/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
